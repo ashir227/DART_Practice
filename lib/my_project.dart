19923105c82e2bb8870338df1869301af00701ef
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-15 21:00:40 +0500

    Add Prac

--- lib/my_project.dart
+++ lib/my_project.dart
@@ -1,8 +1,19 @@
-number() {
-  List sum = [1, 44, 6, 22, 9];
-  print(sum);
+import 'dart:async';
+
+class Car {
+  String? Brand;
+  String? Model;
+  int? year;
+  Car(this.Brand, this.Model, this.year);
+
+  displayinfo() {
+    print("Brand is $Brand");
+    print("Model is $Model");
+    print("Car Year is $year");
+  }
 }
 
 void main() {
-  print(number());
+  Car obj7 = Car("Ferrari", "YFG", 2020);
+  obj7.displayinfo();
 }

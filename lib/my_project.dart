e03feea8f65a912cddd6556b05a424eb9a693157
Ashir Ahmed <ashirahmed810@gmail.com>
2025-08-15 14:45:32 +0500

    Add

--- lib/my_project.dart
+++ lib/my_project.dart
@@ -1,9 +1,10 @@
 void main() {
-  int a = 3;
-  int b = 7;
-  if (a > b) {
-    print("A is Greater");
+  int a = -3;
+  int b = -8;
+
+  if (a < 0) {
+    print("A is negative");
   } else {
-    print("B is Greater");
+    print("A is Positve");
   }
 }
